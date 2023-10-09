75343b641c16bba1010949ed5bfae0e3adb96327
Farino Joshua <96770446+farinojoshua@users.noreply.github.com>
2023-10-09 19:55:47 +0700

    setup deployment

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,24 @@ import { defineConfig } from 'vite';
 import laravel, { refreshPaths } from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: process.env.NODE_ENV === "production" ? "https://" : "/",
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: `build/assets/[name].[hash][extname]`,
+                chunkFileNames: `build/assets/[name].[hash].js`,
+                entryFileNames: `build/assets/[name].[hash].js`,
+            },
+        },
+    },
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/js/calendar.js',
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/calendar.js",
             ],
-            refresh: [
-                ...refreshPaths,
-                'app/Livewire/**',
-            ],
+            refresh: [...refreshPaths, "app/Livewire/**"],
         }),
     ],
 });
